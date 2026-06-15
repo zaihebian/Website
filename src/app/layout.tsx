e00a5de8b018072fb529e588039ba494eb1733b2
Parker <zaihebian@126.com>
2026-06-15 21:09:08 +0100

    Use proxied Mautic tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import "./globals.css";
 
 const geistSans = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-geist-mono" });
-const mauticBaseUrl = (process.env.MAUTIC_BASE_URL ?? "https://mautic.liqentech.com").replace(/\/$/, "");
 
 const siteUrl = "https://liqentech.com";
 const siteName = "LiqenTech";
@@ -174,7 +173,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
               w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
               m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','${mauticBaseUrl}/mtc.js','mt');
+              })(window,document,'script','/api/mautic/mtc.js','mt');
 
               mt('send', 'pageview');
             `,
